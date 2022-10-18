a40acc04806515e8e4546d826eb7293a78582a90
Theofilusarifin <theofilusarifin@gmail.com>
2022-10-19 03:00:22 +0700

    Ui Login

---
+++ 160420046_CoffeeshopTycoon/app/src/main/java/id/ac/ubaya/a160420046_coffeeshoptycoon/PreparationActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.a160420046_coffeeshoptycoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreparationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preparation)
+    }
+}
