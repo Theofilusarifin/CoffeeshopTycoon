b487c7294cc66ded55a29f7876c26a1f2e08d71d
Theofilusarifin <theofilusarifin@gmail.com>
2022-10-18 19:21:15 +0700

    Initial Files

---
+++ app/src/main/java/id/ac/ubaya/theoproject/a160420046_cofeeshoptycoon/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.theoproject.a160420046_cofeeshoptycoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
